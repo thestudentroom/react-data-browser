145657881d72b6349d1dd893547deb5dbea8e035
davidalekna <david.alekna@msknote.com>
2018-09-18 09:03:46 +0100

    updates

---
+++ stories/examples/SimpleTable/HeadCell.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Downshift from 'downshift';
+import { HeadRowItem } from '../styles';
+import HeadCellMenu from './HeadCellMenu';
+
+export const HeadCell = ({ style, render, flex, selected }) => (
+  <Downshift>
+    {({ isOpen, toggleMenu }) => (
+      <div style={{ flex, ...style }}>
+        <HeadRowItem>
+          {render({
+            id: `head-cell-${selected && selected.sortField}`,
+            onClick: toggleMenu,
+            'data-toggle': 'dropdown',
+            'aria-haspopup': 'true',
+            'aria-expanded': isOpen,
+          })}
+          {isOpen && (
+            <HeadCellMenu selected={selected} toggleMenu={toggleMenu} />
+          )}
+        </HeadRowItem>
+      </div>
+    )}
+  </Downshift>
+);
--- stories/examples/SimpleTable/index.js
+++ stories/examples/SimpleTable/index.js
@@ -1,111 +1,82 @@
 import React from 'react';
+import axios from 'axios';
 import DataBrowser from '../../../src';
 import data from './data';
 import columns from './columns';
-import {
-  Table,
-  FixedTableHead,
-  HeadCell,
-  RowOptionsCell,
-  Row,
-  RowItem,
-} from './styles';
 
 // Checkbox, IconButton
 
 const fixedColWidth = 40;
 
-export const SimpleTable = () => (
-  <DataBrowser totalItems={data.length} columns={columns}>
-    {({
-      columnFlex,
-      visibleColumns,
-      selectAllCheckboxState,
-      checkboxState,
-      onSelection,
-      checkboxToggle,
-      viewType,
-    }) => {
-      return (
-        <Table>
-          <FixedTableHead>
-            <HeadCell
-              style={{
-                flex: '0 0 auto',
-                position: 'relative',
-                width: fixedColWidth,
-              }}
-              render={() => (
-                <input
-                  type="checkbox"
-                  position="relative"
-                  checked={selectAllCheckboxState}
-                  onChange={() =>
-                    onSelection({
-                      items: this.state.rows.map(item => item.id),
-                    })
-                  }
-                />
-              )}
-            />
-            {visibleColumns.map((cell, index) => (
-              <HeadCell
-                key={index}
-                selected={cell}
-                flex={columnFlex[index]}
-                render={props => <div {...props}>{cell.label}</div>}
-              />
-            ))}
-            <RowOptionsCell
-              head
-              width={fixedColWidth}
-              render={({ isOpen, ...props }) => (
-                <button {...props} color={isOpen ? 'blue' : '#555'}>
-                  {viewType === 'LIST_VIEW' ? 'view_list' : 'view_module'}
-                </button>
-              )}
-            />
-          </FixedTableHead>
-          <TableBody>
-            {items.map((row, key) => (
-              <Row key={key} selectable>
-                <RowItem style={{ width: fixedColWidth }} flex="0 0 auto">
-                  <input
-                    type="checkbox"
-                    id={row.id}
-                    checked={checkboxState(row.id)}
-                    onChange={() => checkboxToggle({ rowId: row.id })}
-                  />
-                </RowItem>
-                {visibleColumns.map(({ label, sortField, isLocked }, index) => (
-                  <RowItem
-                    key={sortField}
-                    flex={columnFlex[index]}
-                    cursor="pointer"
-                    checked={checkboxState(row.id)}
-                    onClick={() => alert(`🦄 clicked on a row (id) ${row.id}`)}
-                  >
-                    {isLocked && `🔒 `}
-                    {fieldReducer(
-                      getObjectPropertyByString(row, sortField),
-                      sortField,
-                    )}
-                  </RowItem>
+class SimpleTable extends React.Component {
+  state = { rows: [], loading: true };
+  async componentDidMount() {
+    const usersPromise = axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+    const albumsPromise = axios.get(
+      'https://jsonplaceholder.typicode.com/photos?albumId=1',
+    );
+
+    const [users, albums] = await Promise.all([usersPromise, albumsPromise]);
+
+    const rows = users.data.map(user => ({
+      ...user,
+      album: albums.data.find(album => album.id === user.id),
+    }));
+
+    this.setState({ rows, loading: false });
+  }
+  render() {
+    return (
+      <DataBrowser totalItems={data.length} columns={columns}>
+        {({
+          columnFlex,
+          visibleColumns,
+          selectAllCheckboxState,
+          checkboxState,
+          onSelection,
+          checkboxToggle,
+          viewType,
+        }) => {
+          return (
+            <div>
+              <div>
+                {visibleColumns.map((cell, index) => (
+                  <div key={index} selected={cell} flex={columnFlex[index]}>
+                    {cell.label}
+                  </div>
                 ))}
-                <RowOptionsCell
-                  width={fixedColWidth}
-                  checked={checkboxState(row.id)}
-                  render={({ isOpen, ...props }) => (
-                    <button {...props} color={isOpen ? 'white' : '#999'}>
-                      b
-                    </button>
-                  )}
-                />
-              </Row>
-            ))}
-          </TableBody>
-        </Table>
-      );
-    }}
-  </DataBrowser>
-);
+              </div>
+              <div>
+                {data.map((row, key) => (
+                  <div key={key} selectable>
+                    {visibleColumns.map(
+                      ({ label, sortField, isLocked }, index) => (
+                        <div
+                          key={sortField}
+                          flex={columnFlex[index]}
+                          cursor="pointer"
+                          checked={checkboxState(row.id)}
+                          onClick={() =>
+                            alert(`🦄 clicked on a row (id) ${row.id}`)
+                          }
+                        >
+                          {isLocked && `🔒 `}
+                          {fieldReducer(
+                            getObjectPropertyByString(row, sortField),
+                            sortField,
+                          )}
+                        </div>
+                      ),
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        }}
+      </DataBrowser>
+    );
+  }
+}
